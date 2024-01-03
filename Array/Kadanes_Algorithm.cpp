5f7aaa5bf7f94114daf471f81294047a4aa5d07e
Ayush Singh <ayushsingh474020@gmail.com>
2024-01-04 00:07:00 +0530

    Add all my files

---
+++ Array/Kadanes_Algorithm.cpp
@@ -0,0 +1,16 @@
+int maxSubArray(vector<int>& nums) {
+    int maxsum=-pow(2,31);
+    int cursum=0;
+    int i=0;
+    while(i<nums.size()){
+        cursum+=nums[i];
+        if(cursum>maxsum){
+            maxsum=cursum;
+        }
+        if(cursum<0){
+            cursum=0;
+        }
+        i++;
+    }
+    return maxsum;
+    }
---
+++ Array/Pascals_Trianle.cpp
@@ -0,0 +1,16 @@
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> ans;
+    for(int i=0 ; i<numRows ; i++){
+        vector<int> extra;
+        for(int j=0 ; j<=i ; j++){
+            if(j==0 or j==i){
+                extra.push_back(1);
+            }
+            else{
+                extra.push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+        }
+        ans.push_back(extra);
+    }
+    return ans;
+}
